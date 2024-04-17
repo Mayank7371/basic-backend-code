66e26bed160f59712f6be746417d50a9bfb16a97
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-04-17 22:32:41 +0530

    Updated userSchema

--- Backend 1/models/sub_todo.models.js
+++ Backend 1/models/sub_todo.models.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true, 
+    unique: true, // username should be unique
+    lowercase: true, //all username should be lowercase
+  },
 
 export const User = mongoose.model("User", userSchema);
