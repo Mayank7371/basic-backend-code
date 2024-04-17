ccee90b49a1b0820325fe49db4e6d455f26d259a
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-04-18 04:11:45 +0530

    added subtodo schema

--- Backend 1/models/sub_todo.models.js
+++ Backend 1/models/sub_todo.models.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose";
+
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: Syring,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    // now i want to track ki ye field kisne banaya hai
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
 
