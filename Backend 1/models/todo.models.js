ab8f0d73cf22d037c8f9ddaa660d23c55920e7ad
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-04-18 04:12:42 +0530

    Update todo.models.js

--- Backend 1/models/todo.models.js
+++ Backend 1/models/todo.models.js
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean, 
+      defalut: false, 
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User", 
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ], // Aray of subtodos
+  },
+  { timestamps: true }
+);
 
 export const Todo = mongoose.model("Todo", todoSchema);
